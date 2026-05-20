c1b3b9521468c186e5ba633f7547443c4f7ec565
Ignatius Abraham Aristio Kusnadi <abrahamkusnadi@gmail.com>
2026-05-20 10:31:46 +0700

    fix: add admin middleware to secure authentication

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,34 @@
 <?php
 
+use App\Http\Middleware\IsAdmin;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\InvoiceController;
 use App\Http\Controllers\AuthController;
 
-// Home 
-Route::get('/', [ProductController::class, 'index'])->name('home');
-
-// Products 
-Route::resource('products', ProductController::class)->except(['show']);
-
-// Categories
-Route::resource('categories', CategoryController::class)->except(['show']);
-
-// Invoices
-Route::resource('invoices', InvoiceController::class)->middleware('auth');
-Route::post('/invoices/add/{product}', [InvoiceController::class, 'add'])
-    ->name('invoices.add')
-    ->middleware('auth');
-
-
-// Auth
+// Authentication Routes
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-
 Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
 Route::post('/register', [AuthController::class, 'register']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+// Products & Categories CRUD (Must login and has admin role)
+Route::middleware(['auth', IsAdmin::class])->group(function () {
+    Route::resource('products', ProductController::class)->except(['index', 'show']);
+    Route::resource('categories', CategoryController::class)->except(['index','show']);
+});
+
+// Public Route 
+Route::get('/', [ProductController::class, 'index'])->name('home');
+Route::resource('products', ProductController::class)->only(['index', 'show']);
+Route::resource('categories', CategoryController::class)->only(['index','show']);
+
+// Invoices (Must login)
+Route::middleware(['auth'])->group(function () {
+    Route::resource('invoices', InvoiceController::class)->middleware('auth');
+    Route::post('/invoices/add/{product}', [InvoiceController::class, 'add'])->name('invoices.add');
+});
+
+
